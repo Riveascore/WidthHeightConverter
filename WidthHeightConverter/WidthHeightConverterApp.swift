68dcc5f164c8325fe923fcb13418f4ea430fc389
Chris Aga <chrisaga.dev@gmail.com>
2022-12-03 18:02:53 -0600

    Initial Commit

---
+++ WidthHeightConverter/WidthHeightConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidthHeightConverterApp.swift
+//  WidthHeightConverter
+//
+//  Created by Chris Aga on 12/3/22.
+//
+
+import SwiftUI
+
+@main
+struct WidthHeightConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
